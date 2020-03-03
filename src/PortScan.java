d623524121fa50a212c606a18fa7f38b128bf61b
Thad <tshinno@gmail.com>
2020-03-03 16:48:44 -0700

    Compile and organize files into bin and src

--- src/PortScan.java
+++ src/PortScan.java
@@ -3,7 +3,7 @@ import java.net.Socket;
 public class PortScan {
 	public static void main(String[] args) {
 		final int NUM_PORTS = 65535;
-		String ip = args[0]
+		String ip = args[0];
 		
 		Socket s;
 		for (int i = 1; i <= NUM_PORTS; i++) {
