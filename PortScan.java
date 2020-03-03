16ad9a87a360a8e10fa4d9e920a136a580f12691
Thad <tshinno@gmail.com>
2020-03-03 16:43:13 -0700

    Create basic port scanner

--- PortScan.java
+++ PortScan.java
@@ -1,5 +1,19 @@
+import java.net.Socket;
+
 public class PortScan {
 	public static void main(String[] args) {
-		System.out.println("Scanning...");
+		final int NUM_PORTS = 65535;
+		String ip = args[0]
+		
+		Socket s;
+		for (int i = 1; i <= NUM_PORTS; i++) {
+			try {
+				s = new Socket(ip, i);
+				System.out.println("Port open: " + i);
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
