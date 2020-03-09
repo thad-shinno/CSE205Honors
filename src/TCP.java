18450c1b9f471da8d56b3ab5a6886570f0a9390e
Thad <tshinno@gmail.com>
2020-03-09 10:50:41 -0700

    Fix compilation error caused by git

--- src/TCP.java
+++ src/TCP.java
@@ -7,22 +7,12 @@ import java.io.*;
 
 public class TCP {
 	// scan an ip's port num
-<<<<<<< HEAD
 	public static void Scan(String ip, int num) {
 		try {
 			Socket s = new Socket(ip, num);
 			System.out.print("Port Open: " + num);
 		}
 		catch (Exception e) {
-=======
-	public static String Scan(String ip, int num) {
-		try {
-			Socket s = new Socket(ip, num);
-			return "Port Open: " + num;
-		}
-		catch (Exception e) {
-			return null;
->>>>>>> a2ef5e715bc1ec1496768e6a3d312eb37a337794
 		}
 	}
 	
